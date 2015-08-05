802251b1382e678752fbf4733ea33c4d97d8f80e
arun <you@example.com>
2015-08-05 17:50:11 +0530

    Html pages, JS for Enterprise

--- web/enterprise/enterprisecontroller.js
+++ web/enterprise/enterprisecontroller.js
@@ -1,19 +1,20 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller('EnterpriseController', ['$scope', function ($scope) {
-    $scope.showForm = function () {
-$scope.mypopup = true;}
-$scope.enterprises = [];
-      $scope.saveEnterprise = function() {
-$scope.enterprises.push({
-        name: $scope.enterprise.name,
-        contact: $scope.enterprise.contact,
-        address: $scope.enterprise.address,
-        email: $scope.enterprise.email,
-      });
-	$scope.enterprise.name = "";
-	$scope.enterprise.contact = "";
-	$scope.enterprise.address = "";
-	$scope.enterprise.email = "";
-    };
-}]);
+        $scope.showForm = function () {
+            $scope.mypopup = true;
+        }
+        $scope.enterprises = [];
+        $scope.saveEnterprise = function () {
+            $scope.enterprises.push({
+                name: $scope.enterprise.name,
+                contact: $scope.enterprise.contact,
+                address: $scope.enterprise.address,
+                email: $scope.enterprise.email,
+            });
+            $scope.enterprise.name = "";
+            $scope.enterprise.contact = "";
+            $scope.enterprise.address = "";
+            $scope.enterprise.email = "";
+        };
+    }]);
