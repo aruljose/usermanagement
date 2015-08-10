c48c16337ec61a1a9160740755954198ae37c4aa
Firstname Lastname <your.email@example.org>
2015-08-10 11:56:17 +0530

    Added checklist.html and checklist.js files

---
+++ web/sections/checklist.js
@@ -0,0 +1,11 @@
+function ChecklistController($scope) {
+  $scope.checklists = [];
+
+  $scope.addChecklist = function() {
+    $scope.checklists.push({text:$scope.checklistText, done:false});
+    $scope.checklistText = '';
+  };
+}
+
+
+
