8fe5904c2ec26173850597a4178f36c12627111e
Firstname Lastname <your.email@example.org>
2015-08-10 17:10:14 +0530

    Modified document.html, ckeditor.js,ckeditor.js files

--- web/ckeditor/ckeditor.js
+++ web/ckeditor/ckeditor.js
@@ -1,10 +1,8 @@
-var app = angular.module("ckeditorApp", []);
-app.controller("ckEditor", function($scope) {
+/*app.controller("ckEditor", function($scope) {
 $scope.ckeditordata = [];
 $scope.save = function () {
 $scope.ckeditordata = CKEDITOR.instances.editor.getData();
 console.log( CKEDITOR.instances.editor.getData());
-}
-})
+};
+});*/
 
-CKEDITOR.replace( 'editor');
