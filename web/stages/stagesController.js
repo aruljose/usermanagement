c74e57d914906e11369b3944c800053516d29569
praveen <praveen@bl1>
2015-08-11 13:30:10 +0530

    view,js,css for tree added

---
+++ web/stages/stagesController.js
@@ -0,0 +1,21 @@
+angular.module('userManagementApp', [])
+    .controller('TreeController', ['$scope', function($scope,$compile) {
+      
+     /* $scope.click = function () {
+          alert("click");
+          
+      };*/
+      
+      $scope.delete = function(data) {
+        data.nodes = [];
+    };
+    $scope.add = function(data) {
+        var post = data.nodes.length + 1;
+        var newNode = data.name + '-' + post;
+        data.nodes.push({name: newNode,nodes: []});
+    };
+    $scope.tree = [
+        {name: "", 
+         nodes: []}];
+    
+    }]);
