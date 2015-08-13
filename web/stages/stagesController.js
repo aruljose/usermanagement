7285515c8497a2a70518f93e03ed3df33df15200
praveen <praveen@bl1>
2015-08-13 17:27:39 +0530

    stagesview.html file altered

--- web/stages/stagesController.js
+++ web/stages/stagesController.js
@@ -1,17 +1,33 @@
 angular.module('userManagementApp', [])
         .controller('TreeController', ['$scope', function ($scope) {
                 
+             //  var newNode = [];
+              //  var currentCache = [];   
+        
                 $scope.delete = function (data) {
                     data.nodes = [];
                 };
                 $scope.add = function (data) {
-                    var post = data.nodes.length + 1;
-                    var newNode = data.name + post + '.';
+                    
+                    
+                    var post = data.nodes.length + 1  ;
+                    var newNode = data.name + post + '.' ;
                     data.nodes.push({name: newNode, nodes: []});
-                };
+                    // cache = currentCache;
+                     //currentCache.push(cached);
+               
+                
+                if(newNode !== ''){
+                    
+                    var newNode = '';
+                    
+                } };
+               
 
                 $scope.toggle = function () {
-                    $scope.showhide = !$scope.showhide;
+                     $scope.showhide = !$scope.showhide;
+
+                   
                 };
 
                 $scope.tree = [{
@@ -19,4 +35,5 @@ angular.module('userManagementApp', [])
                         nodes: []
 
                     }];
+                
             }]);
