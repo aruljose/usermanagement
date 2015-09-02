bd92813b2c4cf6854a5392b01661b239302349e7
arun <you@example.com>
2015-09-02 17:34:05 +0530

    modified  ckeditor.js, userdetail.cxss, projectdetail.js, userdetail.html, userindex.html

--- web/user/projectdetail.js
+++ web/user/projectdetail.js
@@ -12,56 +12,6 @@ app.controller("ProjectDetailController",function($scope){
 		]
 
         },
-/*{
-            "name": "Design Phase",
-                "sub": [
-		{"name": "System Architecture Document"}, 
-		{"name": "Hardware Architecture Document"}, 
-		{"name": "Software Architecture Document"}, 
-		{"name": "Enclosure Design Document"}, 
-		{"name": "Proof of Concept document"}
-		]
-        },
-
-{
-            
-        },
-{
-            "name": "NPI (New Product Introduction) Phase",
-                "sub": [
-		{"name": "Design for Supply Chain"}, 
-		{"name": "Design for Test Document"}, 
-		{"name": "Design for Assembly Document"}, 
-		{"name": "Design for Manufacturing Document"}, 
-		{"name": "Design for Compliance Document"}
-		]
-        },
-
-{
-            "name": "Manufacturing Phase ",
-                "sub": [
-		{"name": "PCB Fabrication Document"}, 
-		{"name": "Bill of Materials"}, 
-		{"name": "Manufacturing Documents"}, 
-		{"name": "System Assembly Document"}, 
-		{"name": "PCBA testing"},
-		{"name": "System Testing"}
-		]
-        },
-
-{
-            "name": "Support Phase ",
-                "sub": [
-		{"name": "Manufacturing Support"}, 
-		{"name": "Value Engineering"}, 
-		{"name": "Test & Repair Document"}, 
-		{"name": "Homologation Document"}, 
-		{"name": "Disposal/Recycling Documents "}
-		
-		]
-        },*/
-
-
 ]
     };
 
